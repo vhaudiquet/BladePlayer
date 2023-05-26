91c4df92b488a84f9d369cc27d3e624c42138f83
vhaudiquet <vhaudiquet343@hotmail.fr>
2023-05-26 02:12:38 +0200

    Updated everything librespot update fixes Spotify bug (fix #26)

---
+++ app/src/main/java/v/blade/sources/deezer/DeezerPlayer.java
@@ -0,0 +1,55 @@
+package v.blade.sources.deezer;
+
+import v.blade.library.Song;
+import v.blade.sources.Source;
+
+public class DeezerPlayer extends Source.Player
+{
+    @Override
+    public void init()
+    {
+
+    }
+
+    @Override
+    public void play()
+    {
+
+    }
+
+    @Override
+    public void pause()
+    {
+
+    }
+
+    @Override
+    public void playSong(Song song)
+    {
+
+    }
+
+    @Override
+    public void seekTo(long millis)
+    {
+
+    }
+
+    @Override
+    public long getCurrentPosition()
+    {
+        return 0;
+    }
+
+    @Override
+    public long getDuration()
+    {
+        return 0;
+    }
+
+    @Override
+    public boolean isPaused()
+    {
+        return false;
+    }
+}
